57ba49ca30d87a588b689a49384a89ac2b7144b3
Darius Krištapavičius <darius@darneta.lt>
2018-04-07 14:33:25 +0300

    ENV support for variables

--- functions/variables.php
+++ functions/variables.php
@@ -14,29 +14,32 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+function env_var($name, $fallback = null)
+{
+   return array_key_exists($name, $_ENV) ? $_ENV[$name] : $fallback;
+}
+
 $version = 1.3;
-$title = "Certificate Expiry Monitor";
+$title = env_var('CEM_TITLE','Certificate Expiry Monitor');
 
 $current_folder = get_current_folder();
 
 # timeout in seconds
-$timeout = 2;
+$timeout = (int)env_var('CEM_TIMEOUT',2);
 
-date_default_timezone_set('UTC');
+date_default_timezone_set(env_var('CEM_TIMEZONE','UTC'));
 
-ini_set('default_socket_timeout', 2);
+ini_set('default_socket_timeout', $timeout);
 
 $random_blurp = rand(1000,99999);
 
-$current_domain = "certificatemonitor.org";
-$current_link = "certificatemonitor.org";
+$current_domain = env_var('CEM_DOMAIN', 'certificatemonitor.org');
+$current_link = env_var('CEM_LINK', 'certificatemonitor.org');
 
-$slack_webhook = "";
+$slack_webhook = env_var('CEM_SLACK_WEBHOOK', '');
 
 // set this to a location outside of your webroot so that it cannot be accessed via the internets.
 
-$pre_check_file = '/home/certmon/domains/certificatemonitor.org/cert-monitor/pre_checks.json';
-$check_file = '/home/certmon/domains/certificatemonitor.org/cert-monitor/checks.json';
-$deleted_check_file = '/home/certmon/domains/certificatemonitor.org/cert-monitor/deleted_checks.json';
-
-?>
+$pre_check_file = env_var('CEM_PRE_CHECK_FILE','/home/certmon/domains/certificatemonitor.org/cert-monitor/pre_checks.json');
+$check_file = env_var('CEM_CHECK_FILE','/home/certmon/domains/certificatemonitor.org/cert-monitor/checks.json');
+$deleted_check_file = env_var('CEM_DELETED_CHECK_FILE', '/home/certmon/domains/certificatemonitor.org/cert-monitor/deleted_checks.json');
